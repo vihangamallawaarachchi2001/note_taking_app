e0d3ca12c38370ca4b2d0281afb202be15805206
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-02-16 00:24:22 +0530

    ui: Login Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import Login from "./_components/Login";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -16,6 +18,12 @@ export default async function Home() {
     <HydrateClient>
       <div >
       <div className="absolute bottom-0 left-0 h-full w-full rounded-full bg-[#0d3b4b] opacity-40 blur-3xl"></div>
+      <header>
+        <Image src="/twc.svg" alt="" width={100} height={30} />
+      </header>
+      <section>
+        <Login />
+      </section>
       </div>
     </HydrateClient>
   );
