bc6308237891ab2b8c31c9e71af3783e76abe09b
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-02-16 08:12:35 +0530

    chore: cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import Login from "./_components/Login";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
@@ -16,14 +12,14 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <div >
-      <div className="absolute -z-10 bottom-0 left-0 h-full w-full rounded-full bg-[#0d3b4b] opacity-40 blur-3xl"></div>
-      <header>
-        <Image src="/twc.svg" alt="" width={100} height={30} />
-      </header>
-      <section>
-        <Login />
-      </section>
+      <div>
+        <div className="absolute bottom-0 left-0 -z-10 h-full w-full rounded-full bg-[#0d3b4b] opacity-40 blur-3xl"></div>
+        <header>
+          <Image src="/twc.svg" alt="" width={100} height={30} />
+        </header>
+        <section>
+          <Login />
+        </section>
       </div>
     </HydrateClient>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import Signup from "../_components/Signup";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
